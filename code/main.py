459567c0265aef3dedb96aa44d8ac9b9889ecc3b
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-04-11 09:32:11 +0400

    заглушка

--- code/main.py
+++ code/main.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI, Response
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
+from fastapi.responses import HTMLResponse
 
 import os
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/api", tags=["base link"])
-def root():
-	return {"status" : "Ok"}
+@app.get("/", tags=["base link"])
+async def root():
+    return """
+    <html>
+        <head>
+            <title>Сайт седьмого стёта проектировщиков</title>
+        </head>
+        <body>
+            <h1>На данный момент сайт находится в разработке</h1>
+        </body>
+    </html>
+    """

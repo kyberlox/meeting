183b41bcf503056b6fc8ec4afdb9228f016c92b2
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-04-14 12:23:16 +0400

    front

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -18,8 +18,8 @@ export default defineConfig({
     },
   },
   server: {
-    host: "0.0.0.0",
-    port: 4173,
+    host: "meeting.mosckba.ru",
+    port: 5173,
   },
   css: {
     preprocessorOptions: {

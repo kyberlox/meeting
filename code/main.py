f6c01cb8cfa1c4b5f001972417c404515be8e40d
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-04-15 12:04:34 +0400

    mp4

--- code/main.py
+++ code/main.py
@@ -24,7 +24,7 @@ async def root():
 
 @app.get("/api/video/{name}", tags=["video"])
 async def stream_video(name : str):
-    path = "/app/static/{name}"
+    path = f"/app/static/{name}"
     file_like  = open(path, mode="rb")
 
     return StreamingResponse(iterfile(), media_type="video/mp4")

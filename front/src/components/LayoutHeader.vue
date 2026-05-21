9523950fa596ad2a30ec017f474af9cce46ee60d
Fullofb3ans <igorgazinskii6@gmail.com>
2026-05-21 16:02:24 +0400

    iss/text-upd

--- front/src/components/LayoutHeader.vue
+++ front/src/components/LayoutHeader.vue
@@ -1,65 +1,77 @@
 <template>
-    <header>
-        <div class="header bg-white shadow-md">
-            <div
-                 class="header__wrapper max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-3 sm:py-4 flex flex-col sm:flex-row justify-between items-center">
+<header>
+    <div class="header bg-white shadow-md">
+        <div
+             class="header__wrapper max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-3 sm:py-4 flex flex-col sm:flex-row justify-between items-center">
+            <div class="flex flex-row gap-[30px] items-center">
+                <div class="header__logo__wrapper mb-3 sm:mb-0">
+                    <img src="@/assets/logoSar.png"
+                         alt="logo"
+                         class="h-8 sm:h-10 md:h-12 w-auto">
+                </div>
                 <div class="header__logo__wrapper mb-3 sm:mb-0">
                     <a href="https://mosckba.ru/"
                        target="_blank"
                        class="header__logo">
-                        <img src="@/assets/logo.png"
+                        <img src="@/assets/logoMosckba.png"
                              alt="logo"
                              class="h-8 sm:h-10 md:h-12 w-auto">
                     </a>
                 </div>
-                <nav class="w-full sm:w-auto">
-                    <ul
-                        class="header__navbar flex flex-wrap justify-center sm:justify-end items-center space-x-2 sm:space-x-4 md:space-x-8">
-                        <li v-for="(nav) in navbarLinks"
-                            :key="'nav' + nav.id"
-                            class="header__navbar__item cursor-pointer my-1">
-                            <div v-if="!nav.subpoints"
-                                 :to="nav.route"
-                                 @click="handleScrollTabs(nav.route)"
-                                 class="text-theme-grey-blue hover:text-theme-blue-dark font-medium transition-colors duration-200 text-sm sm:text-base px-2 py-1 rounded hover:bg-gray-100"
-                                 active-class="text-theme-blue-dark font-bold border-b-2 border-theme-orange">
-                                {{ nav.title }}
-                            </div>
-                            <div v-else
-                                 class="relative group"
-                                 @mouseenter="handleSubpoints(true, nav.id)"
-                                 @mouseleave="handleSubpoints(false, nav.id)">
-                                <div
-                                     class="text-theme-grey-blue hover:text-theme-blue-dark font-medium transition-colors duration-200 text-sm sm:text-base px-2 py-1 rounded hover:bg-gray-100 cursor-pointer flex items-center">
-                                    {{ nav.title }}
-                                    <svg xmlns="http://www.w3.org/2000/svg"
-                                         class="h-3 w-3 sm:h-4 sm:w-4 ml-1"
-                                         fill="none"
-                                         viewBox="0 0 24 24"
-                                         stroke="currentColor">
-                                        <path stroke-linecap="round"
-                                              stroke-linejoin="round"
-                                              stroke-width="2"
-                                              d="M19 9l-7 7-7-7" />
-                                    </svg>
-                                </div>
-                                <div :class="{ 'opacity-100': subPointActive && activeIndex == nav.id }"
-                                     class="absolute opacity-0 left-0 w-full bg-white rounded-md shadow-lg z-10">
-                                    <RouterLink v-for="point in nav.subpoints"
-                                                :key="point + 'archive'"
-                                                :to="`/archive/${point}`"
-                                                class="block rounded-sm px-3 sm:px-4 py-1 sm:py-2 text-xs sm:text-sm text-theme-grey-blue hover:text-theme-blue-dark hover:bg-gray-100"
-                                                active-class="text-theme-blue-dark font-bold border-l-2 border-theme-orange bg-gray-50">
-                                        {{ point }}
-                                    </RouterLink>
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
-                </nav>
+                <div class="header__logo__wrapper mb-3 sm:mb-0">
+                    <img src="@/assets/logoNpa.png"
+                         alt="logo"
+                         class="h-8 sm:h-10 md:h-10 w-auto">
+                </div>
             </div>
+            <nav class="w-full sm:w-auto">
+                <ul
+                    class="header__navbar flex flex-wrap justify-center sm:justify-end items-center space-x-2 sm:space-x-4 md:space-x-8">
+                    <li v-for="(nav) in navbarLinks"
+                        :key="'nav' + nav.id"
+                        class="header__navbar__item cursor-pointer my-1">
+                        <div v-if="!nav.subpoints"
+                             :to="nav.route"
+                             @click="handleScrollTabs(nav.route)"
+                             class="text-theme-grey-blue hover:text-theme-blue-dark font-medium transition-colors duration-200 text-sm sm:text-base px-2 py-1 rounded hover:bg-gray-100"
+                             active-class="text-theme-blue-dark font-bold border-b-2 border-theme-orange">
+                            {{ nav.title }}
+                        </div>
+                        <div v-else
+                             class="relative group"
+                             @mouseenter="handleSubpoints(true, nav.id)"
+                             @mouseleave="handleSubpoints(false, nav.id)">
+                            <div
+                                 class="text-theme-grey-blue hover:text-theme-blue-dark font-medium transition-colors duration-200 text-sm sm:text-base px-2 py-1 rounded hover:bg-gray-100 cursor-pointer flex items-center">
+                                {{ nav.title }}
+                                <svg xmlns="http://www.w3.org/2000/svg"
+                                     class="h-3 w-3 sm:h-4 sm:w-4 ml-1"
+                                     fill="none"
+                                     viewBox="0 0 24 24"
+                                     stroke="currentColor">
+                                    <path stroke-linecap="round"
+                                          stroke-linejoin="round"
+                                          stroke-width="2"
+                                          d="M19 9l-7 7-7-7" />
+                                </svg>
+                            </div>
+                            <div :class="{ 'opacity-100': subPointActive && activeIndex == nav.id }"
+                                 class="absolute opacity-0 left-0 w-full bg-white rounded-md shadow-lg z-10">
+                                <RouterLink v-for="point in nav.subpoints"
+                                            :key="point + 'archive'"
+                                            :to="`/archive/${point}`"
+                                            class="block rounded-sm px-3 sm:px-4 py-1 sm:py-2 text-xs sm:text-sm text-theme-grey-blue hover:text-theme-blue-dark hover:bg-gray-100"
+                                            active-class="text-theme-blue-dark font-bold border-l-2 border-theme-orange bg-gray-50">
+                                    {{ point }}
+                                </RouterLink>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </nav>
         </div>
-    </header>
+    </div>
+</header>
 </template>
 
 <script lang="ts">
